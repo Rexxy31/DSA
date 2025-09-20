0fe801db02ab2854477c540ddd3e85476d2c7881
Yogesh Kumar <80209601+Rexxy31@users.noreply.github.com>
2025-09-20 10:51:46 +0000

    20-09-2025

--- DSA.java
+++ DSA.java
@@ -1,14 +1,17 @@
-import java.util.HashSet;
-import java.util.Set;
+import java.util.HashMap;
 
 public class DSA {
     public static void main(String[] args) {
 
-        int[] A = {1, 2, 3, 4, 5};
-        int[] B = {-4, -3, 2, 3, -2};
+        // int[] A = {1, 2, 3, 4, 5};
+        // int[] B = {-4, -3, 2, 3, -2};
+        // int[] C = {7,1,5,3,6,4};
         // System.out.println(binarySearch(A, 6));
         // System.out.println(closestToZero(B));
-        System.out.println(mergeAlternately("abc", "pqrst"));
+        // System.out.println(mergeAlternately("abc", "pqrst"));
+        // System.out.println(romanToInt("MCMXCIV"));
+        // System.out.println(isSubsequence("abc", "apqcrs"));
+        // System.out.println(maxProfit(C));
 
 
     }
@@ -91,4 +94,71 @@ public class DSA {
         return s.toString();
 
     }
+
+    public static int romanToInt(String s) {
+
+        HashMap<Character, Integer> d = new HashMap<>();
+        d.put('I', 1);
+        d.put('V', 5);
+        d.put('X', 10);
+        d.put('L', 50);
+        d.put('C', 100);
+        d.put('D', 500);
+        d.put('M', 1000);
+
+        int summ = 0;
+        int n = s.length();
+        int i = 0;
+
+        while (i < n) {
+            if (i < n - 1 && d.get(s.charAt(i)) < d.get(s.charAt(i+1)) ) {
+                summ += d.get(s.charAt(i+1)) - d.get(s.charAt(i));
+                i += 2;
+            } else {
+                summ += d.get(s.charAt(i));
+                i += 1;
+            }
+        }
+        return summ;
+    }
+
+
+    public static boolean isSubsequence(String s, String t) {
+        int S = s.length();
+        int T = t.length();
+
+        int sp = 0;
+        int tp = 0;
+
+        while (sp < S && tp < T) {
+            if (s.charAt(sp) == t.charAt(tp)) {
+                sp++;
+            }
+            tp++;
+        }
+
+        return sp == S;
+    }
+
+    public static int maxProfit(int[] prices) {
+        float min_price = Integer.MAX_VALUE;
+        int max_profit = 0;
+
+        for (int price : prices) {
+            if (price < min_price) {
+                min_price = price;
+            }
+
+            int profit = (int) (price - min_price);
+
+            if (profit > max_profit) {
+                max_profit = profit;
+            }
+        }
+
+        return max_profit;
+    }
+
+    
+
 }
