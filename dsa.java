b0baadc478f81c92f708a0c5646390dd7562de94
Yogesh Kumar <80209601+Rexxy31@users.noreply.github.com>
2025-10-14 19:49:40 +0000

    14-10-2025

--- dsa.java
+++ dsa.java
@@ -808,4 +808,25 @@ public class dsa {
         return slow;
     }
     
+    // Definition for a binary tree node.
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    private static TreeNode invertTree(TreeNode root) {
+        if (root == null) return null;
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+        root.left = right;
+        root.right = left;
+        return root;
+    }
 }
--- dsa.py
+++ dsa.py
@@ -673,3 +673,21 @@ def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
         fast = fast.next
 
     return slow
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+    if not root:
+        return None
+
+    root.left, root.right = root.right, root.left
+
+    self.invertTree(root.left)
+    self.invertTree(root.right)
+
+    return root
